726280d189e7556566c4f989a3940bb4581d2f4d
Vipul Verma <vipulvermalm8@gmail.com>
2025-02-01 12:54:53 +0530

    routes created

---
+++ routes/faqRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { deleteFAQ, getFAQ, postFAQ } from '../controllers/faqController'
+
+const router=express.Router()
+
+
+router.route('/getfaq').get(getFAQ)
+router.route('/postfaq').post(postFAQ)
+router.route('/deletefaq/:id').delete(deleteFAQ)
+
+export default router
