7fb9c6f35846dc05441f5d873e94dab03ce06b9b
Vipul Verma <vipulvermalm8@gmail.com>
2025-02-01 17:41:10 +0530

    updated Redis

--- config/Redis.js
+++ config/Redis.js
@@ -1,17 +1,17 @@
 import Redis from 'ioredis';
 
-const redis=new Redis({
+const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
 })
 
-redis.on('connect',()=>{
+redis.on('connect', () => {
     console.log('Redis connected')
 })
 
-redis.on('error',(err)=>{
+redis.on('error', (err) => {
     console.log(err)
 })
 
