4ff67b9d3db53e8b1a5b6746b427759ac725b6ae
Vipul Verma <vipulvermalm8@gmail.com>
2025-02-01 09:46:48 +0530

    updated app file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from "express";
 
 import {config} from 'dotenv';
 
+import ErrorMiddleware from "./middlewares/Error.js";
+
+
 config({
     path:'./config.env'
 })
@@ -9,4 +12,17 @@ config({
 
 const app=express()
 
+
+import faq from './routes/faqRoutes.js'
+
+app.use('/api/v1',faq)
+
 export default app
+
+app.get('/',(req,res)=>
+    res.send(
+        `<h1> click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`
+    )
+)
+
+app.use(ErrorMiddleware)
