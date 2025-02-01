6f3f43df00788607d313f2513bf51b7cf457d0ea
Vipul Verma <vipulvermalm8@gmail.com>
2025-02-01 17:51:13 +0530

    modified app file

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 import express from "express";
-
 import {config} from 'dotenv';
-
+import cors from 'cors';
+import bodyParser from "body-parser";
 import ErrorMiddleware from "./middlewares/Error.js";
 
-
-config({
-    path:'./config.env'
-})
-
+config({ path:'./config.env' })
 
 const app=express()
 
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 import faq from './routes/faqRoutes.js'
-
 app.use('/api/v1',faq)
 
 export default app
